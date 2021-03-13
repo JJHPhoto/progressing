bdcd7484594f38fd5f084313ea289a6d62e47bef
Joshua J. Haller <phone@joshuajhaller.com>
2021-03-13 11:36:51 -0800

    Adding user authentication.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
+import { useAuthTokenStore } from "./utils/auth";
 
 function App() {
+  useAuthTokenStore();
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      testText
+      <h1>testText</h1>
     </div>
   );
 }
