6d5910ba28e9aa3e0d6c4c232c037731ff769bb8
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2021-03-15 19:35:40 -0700

    added empty database seeding script

---
+++ scripts/seedDB.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
+
+const goalSeed = [
+  
+];
+
+db.Goals
+  .remove({})
+  .then(() => db.Goals.collection.insertMany(goalSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
