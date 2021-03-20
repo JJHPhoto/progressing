4dda4634d48c27a3c023a7d077cb16cffbad5a95
Bryson Palmer <brysonpalmer@gmail.com>
2021-03-20 11:50:53 -0700

    Used location in components/PrivateRoute.js to fix an error.

--- models/Goals.js
+++ models/Goals.js
@@ -37,12 +37,28 @@ const GoalSchema = new Schema({
     ],
     user_id: String,
     notes: String,
-}, 
-// {
-//     toJSON: { virtuals: true }
-// }
+}, {
+    toJSON: { virtuals: true }
+});
 
-console.log("in the schema")
-);
+// GoalSchema.virtual("totalMilestoneActions").get( function () {
+//     let totalMilestoneActions = 0;
+//     this.milestones.forEach( function (milestone) {
+//         if ( milestone.actionItems.length > 0 ) {
+//             totalMilestoneActions += 
+//         } else {
+
+//         }
+//     })
+// });
+
+// GoalSchema.virtual("goalSummary").get( function () {
+//     let goalSummary = 0;
+//     this.milestones.forEach( function (milestone) {
+//         if ( milestones.actionItems.length > 0 ) {
+//             goalSummary += 
+//         }
+//     })
+// });
 
 module.exports = Goals = mongoose.model("goals", GoalSchema);
