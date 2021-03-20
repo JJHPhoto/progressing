d418ca5eba5094b72910c165216e46431cdc6693
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2021-03-20 14:02:41 -0700

    fixed navbar links to no longer show anchor errors

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -16,15 +16,15 @@ function NavBar() {
     <div className="mt-4">
       <Navbar bg="success" variant="dark">
       <NavDropdown title={<FontAwesomeIcon icon={faBars}/>} id="basic-nav-dropdown">
-        <NavDropdown.Item><Link to="/home" style={{ textDecoration: 'none', color: 'black'}}>Home</Link></NavDropdown.Item>
-        <NavDropdown.Item><Link to="/creategoal" style={{ textDecoration: 'none', color: 'black'}}>Create Goal</Link></NavDropdown.Item>
+        <NavDropdown.Item as={Link} to="/home" style={{ textDecoration: 'none', color: 'black'}}>Home</NavDropdown.Item>
+        <NavDropdown.Item as={Link} to="/creategoal" style={{ textDecoration: 'none', color: 'black'}}>Create Goal</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item><LogoutButton /></NavDropdown.Item>
       </NavDropdown>
         <Navbar.Collapse className="justify-content-end">
           {user &&
           <Navbar.Text>
-            Welcome, <Link to="/home" style={{ textDecoration: 'none', color: 'white'}} >{user.firstName}!</Link>
+            Welcome, <Link as={Link} to="/home" style={{ textDecoration: 'none', color: 'white'}} >{user.firstName}!</Link>
           </Navbar.Text>}
         </Navbar.Collapse>
       </Navbar>
