c50625f73a6fddd7d97567c53934407fc1c54e02
Joshua J. Haller <phone@joshuajhaller.com>
2021-03-27 22:18:15 -0700

    fixing other ref.

--- client/src/components/StartHeader/index.js
+++ client/src/components/StartHeader/index.js
@@ -1,15 +1,12 @@
 import React from "react";
-import Banner from "../photos/ProgBannerFull.jpg";
+// import Banner from "../photos/ProgBannerFull.jpg";
 import { Link } from "react-router-dom";
 import { Container, Image } from "react-bootstrap";
 
 export function StartHeader() {
   return (
     <Container>
-      <Link to="/home">
-        {" "}
-        <Image src={Banner} />{" "}
-      </Link>
+      <Link to="/home"> {/* <Image src={Banner} />{" "} */}</Link>
     </Container>
   );
 }
