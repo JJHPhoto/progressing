d368758b586592f2ef92d4251d785f45dcc29153
Tskading <Tskading@gmail.com>
2021-03-29 11:45:36 -0700

    Replaced noGoalsCard, minor styling improvements

--- client/src/components/NoGoalsCard/index.js
+++ client/src/components/NoGoalsCard/index.js
@@ -1,32 +1,58 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import GoalImg from "../photos/SampleGoal.png"
+import "./style.css";
+
 
 function NoGoalsCard() {
 
     return (
         <Card className="mb-5" >          
 
-            <Card.Header className="mb-3 pt-3" style={{height: "8rem"}}>
-                <h1 style={{fontSize: "1.25rem"}}>
+            <Card.Header className="mb-1 p-4">
+                <div className="">
+                <h1 className="noGoalHeader" style={{fontSize: "1.1rem"}}>
                     Welcome!  Lets get you started by creating your first goal, click the button below!
                 </h1>
-
-                <p>
-                    Below is a representation of what your goal will look like when you are done.
-                </p>
+                </div>
             </Card.Header>
+            <Card.Body>
 
-             <Card.Img src={GoalImg} />
+                <Row >
+                    <ul className="ml-4 mx-4 font-weight-light"> 
+                    Remember you're S.M.A.R.T - specific, measurable, attainable, relevant, time-bound! 
+                    </ul>
 
-            <Button className="mt-5" style={{backgroundColor: "#01a35a", border: "none"}} >
+                    <ul className="ml-4 mx-4 font-weight-light">
+
+                        As your creating your goal, think of the milestones you will check off to complete it.  This gives you a way to break your goal down into managable, actionable pieces.
+
+                    </ul>
+
+                    <br>
+                    </br>
+
+                    <ul className="ml-4 mx-4 font-weight-light">
+                        We will guide you through the process, its easy!  Click below and see how you will thrive in Progressing.
+                    </ul>
+
+
+
+                </Row>
+
+                <Button className="mt-5" style={{backgroundColor: "#01a35a", border: "none", float: "right"}} >
                 <Link className="text-white" to="/createGoal" >
-                    <p className="pt-2" >
+                    <p className="mt-3" >
                         Click Here to Make Your First Goal!
                     </p>
                 </Link>
             </Button>
+
+            </Card.Body>
+
+           
+
+
         </Card>
 
     );
