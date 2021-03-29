1b828f301e0e21085e80d821f000c4c43f502721
Tskading <Tskading@gmail.com>
2021-03-29 15:15:20 -0700

    media queries for NoGoals component

--- client/src/components/NoGoalsCard/index.js
+++ client/src/components/NoGoalsCard/index.js
@@ -9,21 +9,21 @@ function NoGoalsCard() {
     return (
         <Card className="mb-5" >          
 
-            <Card.Header className="mb-1 p-4">
+            <Card.Header className="ngHeader mb-1 p-4">
                 <div className="">
                 <h1 className="noGoalHeader" style={{fontSize: "1.1rem"}}>
-                    Welcome!  Lets get you started by creating your first goal, click the button below!
+                    Lets get you started by creating your first goal, click the button below!
                 </h1>
                 </div>
             </Card.Header>
             <Card.Body>
 
                 <Row >
-                    <ul className="ml-4 mx-4 font-weight-light"> 
+                    <ul className="noGoal ml-4 mx-4 font-weight-light"> 
                     Remember you're S.M.A.R.T - specific, measurable, attainable, relevant, time-bound! 
                     </ul>
 
-                    <ul className="ml-4 mx-4 font-weight-light">
+                    <ul className="noGoal ml-4 mx-4 font-weight-light">
 
                         As your creating your goal, think of the milestones you will check off to complete it.  This gives you a way to break your goal down into managable, actionable pieces.
 
@@ -32,7 +32,7 @@ function NoGoalsCard() {
                     <br>
                     </br>
 
-                    <ul className="ml-4 mx-4 font-weight-light">
+                    <ul className="noGoal ml-4 mx-4 font-weight-light">
                         We will guide you through the process, its easy!  Click below and see how you will thrive in Progressing.
                     </ul>
 
@@ -40,7 +40,7 @@ function NoGoalsCard() {
 
                 </Row>
 
-                <Button className="mt-5" style={{backgroundColor: "#01a35a", border: "none", float: "right"}} >
+                <Button className="noGoalsButton mt-5" style={{backgroundColor: "#01a35a", border: "none", float: "right"}} >
                 <Link className="text-white" to="/createGoal" >
                     <p className="mt-3" >
                         Click Here to Make Your First Goal!
